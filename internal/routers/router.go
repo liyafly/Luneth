50aa1992935fef2a06604e5a1c0b27db55456f82
liyafly <liyafly@liyaflydeMacBook-Pro.local>
2023-07-07 18:49:25 +0800

    处理文件和标签

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -1,25 +1,30 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	v1 "github.com/liyafly/Luneth/internal/routers/api/v1"
+)
 
 func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	article := v1.NewArticle()
+	tag := v1.NewTag()
 	apiV1 := r.Group("/api/v1")
 	{
-		apiV1.POST("tags")
-		apiV1.DELETE("/tags/:id")
-		apiV1.PUT("/tags/:id")
-		apiV1.PATCH("/tags/:id/state")
-		apiV1.GET("/tags")
+		apiV1.POST("tags", tag.Create)
+		apiV1.DELETE("/tags/:id", tag.Delete)
+		apiV1.PUT("/tags/:id", tag.Update)
+		apiV1.PATCH("/tags/:id/state", tag.Update)
+		apiV1.GET("/tags", tag.List)
 
-		apiV1.POST("articles")
-		apiV1.DELETE("/articles/:id")
-		apiV1.PUT("/articles/:id")
-		apiV1.PATCH("/articles/:id/state")
-		apiV1.GET("/articles/:id")
-		apiV1.GET("/articles")
+		apiV1.POST("articles", article.Create)
+		apiV1.DELETE("/articles/:id", article.Delete)
+		apiV1.PUT("/articles/:id", article.Update)
+		apiV1.PATCH("/articles/:id/state", article.Update)
+		apiV1.GET("/articles/:id", article.Get)
+		apiV1.GET("/articles", article.List)
 
 	}
 	return r
