46a2d4aa33d3b5f36c84e844640a973763952920
liyafly <liyafly@liyaflydeMacBook-Pro.local>
2023-07-08 15:42:46 +0800

    add 配置管理

---
+++ global/setting.go
@@ -0,0 +1,10 @@
+package global
+
+import "github.com/liyafly/Luneth/pkg/setting"
+
+// a global variable
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	DatabaseSetting *setting.DatabaseSettingS
+)
--- main.go
+++ main.go
@@ -1,15 +1,65 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/liyafly/Luneth/global"
+	"github.com/liyafly/Luneth/internal/routers"
+	"github.com/liyafly/Luneth/pkg/setting"
+	"log"
+	"net/http"
+	"time"
+)
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+}
+
+func setupSetting() error {
+	s, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	err := r.Run()
+	//r := gin.Default()
+	//r.GET("/ping", func(c *gin.Context) {
+	//	c.JSON(200, gin.H{
+	//		"message": "pong",
+	//	})
+	//})
+	//err := r.Run()
+	//if err != nil {
+	//	return
+	//}
+	gin.SetMode(global.ServerSetting.RunMode)
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":" + global.ServerSetting.HttpPort,
+		Handler:        router,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+	err := s.ListenAndServe()
 	if err != nil {
 		return
 	}
