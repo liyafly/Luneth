2ed754cee6301c2acc89b1395c11e90874307b48
liyafly <liyafly@liyaflydeMacBook-Pro.local>
2023-07-09 11:17:07 +0800

    gorm 引入和数据库处理

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,5 +1,15 @@
 package model
 
+import (
+	"fmt"
+	"github.com/liyafly/Luneth/global"
+	"github.com/liyafly/Luneth/pkg/setting"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+)
+
 type Model struct {
 	ID         uint32 `gorm:"primary_key" json:"id"`
 	CreatedBy  string `json:"created_by"`
@@ -12,4 +22,31 @@ type Model struct {
 
 type TableNamed interface {
 	TableName() string
+}
+
+func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
+	s := "%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local"
+	dsn := fmt.Sprintf(s,
+		databaseSetting.UserName,
+		databaseSetting.Password,
+		databaseSetting.Host,
+		databaseSetting.DBName,
+		databaseSetting.Charset,
+		databaseSetting.ParesTime,
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	if global.ServerSetting.RunMode == "debug" {
+		db.Logger.LogMode(logger.Info)
+	}
+	db.Config.NamingStrategy = schema.NamingStrategy{SingularTable: true}
+	mysqlDB, e := db.DB()
+	if e != nil {
+		return nil, e
+	}
+	mysqlDB.SetMaxIdleConns(databaseSetting.MaxIdleConns)
+	mysqlDB.SetMaxOpenConns(databaseSetting.MaxOpenConns)
+	return db, nil
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/liyafly/Luneth/global"
+	"github.com/liyafly/Luneth/internal/model"
 	"github.com/liyafly/Luneth/internal/routers"
 	"github.com/liyafly/Luneth/pkg/setting"
 	"log"
@@ -14,6 +15,10 @@ func init() {
 	err := setupSetting()
 	if err != nil {
 		log.Fatalf("init.setupSetting err: %v", err)
+	}
+	err = setupDBEngine()
+	if err != nil {
+		log.Fatalf("init.setupDBEngine err: %v", err)
 	}
 }
 
@@ -39,17 +44,17 @@ func setupSetting() error {
 	return nil
 }
 
+func setupDBEngine() error {
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
-	//r := gin.Default()
-	//r.GET("/ping", func(c *gin.Context) {
-	//	c.JSON(200, gin.H{
-	//		"message": "pong",
-	//	})
-	//})
-	//err := r.Run()
-	//if err != nil {
-	//	return
-	//}
+
 	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 	s := &http.Server{
@@ -63,4 +68,5 @@ func main() {
 	if err != nil {
 		return
 	}
+
 }
